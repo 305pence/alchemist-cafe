8f804237c6fe2c03aea89e030fc20b2959ed30a2
Spencer <spernernicks@icloud.com>
2025-01-16 18:45:57 -0700

    renamed mod to AlchemistCafe

--- src/main/java/io/github/_305pence/alchemistcafe/AlchemistCafe.java
+++ src/main/java/io/github/_305pence/alchemistcafe/AlchemistCafe.java
@@ -1,4 +1,4 @@
-package io.github._305pence.examplemod;
+package io.github._305pence.alchemistcafe;
 
 import org.slf4j.Logger;
 
@@ -35,8 +35,8 @@ import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
+@Mod(AlchemistCafe.MODID)
+public class AlchemistCafe
 {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "examplemod";
@@ -69,7 +69,7 @@ public class ExampleMod
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public ExampleMod(IEventBus modEventBus, ModContainer modContainer)
+    public AlchemistCafe(IEventBus modEventBus, ModContainer modContainer)
     {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
