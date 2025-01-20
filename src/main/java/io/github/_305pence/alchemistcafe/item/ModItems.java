617e08bdadb7765c54eed98127e1500e290ce149
Spencer <spernernicks@icloud.com>
2025-01-20 01:07:31 -0700

    move example item and replace it with soda bottle

---
+++ src/main/java/io/github/_305pence/alchemistcafe/item/ModItems.java
@@ -0,0 +1,11 @@
+package io.github._305pence.alchemistcafe.item;
+
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.Item;
+import net.neoforged.neoforge.registries.DeferredItem;
+
+import static io.github._305pence.alchemistcafe.AlchemistCafe.ITEMS;
+
+public class ModItems {
+    public static final DeferredItem<Item> SODA_BOTTLE = ITEMS.registerSimpleItem("soda_bottle",new Item.Properties());
+}
